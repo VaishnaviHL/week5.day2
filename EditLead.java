30355efc5a5c2f5f0068c97e1c27a3e2817651d3
VaishnaviHL <103865856+VaishnaviHL@users.noreply.github.com>
2022-05-26 23:23:41 +0530

    Add files via upload

---
+++ EditLead.java
@@ -0,0 +1,34 @@
+package week5.day2;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class EditLead extends ProjectSpecificMethods {
+	@Test(dataProvider="testData")
+
+	public void runEditLead(String username, String password, String phoneNumber,String cname) throws InterruptedException {
+		driver.findElement(By.id("username")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//span[text()='Phone']")).click();
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys(phoneNumber);
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a")).click();
+		driver.findElement(By.linkText("Edit")).click();
+		driver.findElement(By.id("updateLeadForm_companyName")).sendKeys(cname);
+		driver.findElement(By.name("submitButton")).click();
+	}
+
+	@DataProvider
+	public String[][] testData() throws IOException {
+		String[][] data = Assignment_ReadExcelData.readData("tc004");
+		return data;
+	}
+}
